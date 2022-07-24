74816218612271de511afb3f433c195e029fa145
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-07-24 01:03:39 -0400

    finished board and passed all tests.

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,54 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        for (int i = 0; i < 3; i++) {
+            if (this.matrix[i][0] == 'X' && this.matrix[i][0] == this.matrix[i][1] && this.matrix[i][1] == this.matrix[i][2]) {
+                return (this.matrix[i][0] == 'X');
+            }
+            if (this.matrix[0][i] == 'X' && this.matrix[0][i] == this.matrix[1][i] && this.matrix[1][i] == this.matrix[2][i]) {
+                return (matrix[0][i] == 'X');
+            }
+        }
+        if (this.matrix[0][0] == 'X' && this.matrix[0][0] == this.matrix [1][1] && this.matrix[1][1] == this.matrix[2][2]){
+            return true;
+        }
+        return this.matrix[0][2] == 'X' && this.matrix[0][2] == this.matrix[1][1] && this.matrix[1][1] == this.matrix[2][0];
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        for (int i = 0; i < 3; i++) {
+            if (this.matrix[i][0] == 'O' && this.matrix[i][0] == this.matrix[i][1] && this.matrix[i][1] == this.matrix[i][2]) {
+                return (this.matrix[i][0] == 'O');
+            }
+            if (this.matrix[0][i] == 'O' && this.matrix[0][i] == this.matrix[1][i] && this.matrix[1][i] == this.matrix[2][i]) {
+                return (matrix[0][i] == 'O');
+            }
+        }
+        if (this.matrix[0][0] == 'O' && this.matrix[0][0] == this.matrix [1][1] && this.matrix[1][1] == this.matrix[2][2]){
+            return true;
+        }
+        return this.matrix[0][2] == 'O' && this.matrix[0][2] == this.matrix[1][1] && this.matrix[1][1] == this.matrix[2][0];
     }
 
     public Boolean isTie() {
-        return null;
+        return !isInFavorOfX() && !isInFavorOfO();
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfO()){
+            return "O";
+        }
+        if (isInFavorOfX()){
+            return "X";
+        }
+        else
+            return "";
     }
-
 }
